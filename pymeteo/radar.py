ee236948f44f36593f7d224b13c2541ad0151a11
Casey Webster <casey.webster@gmail.com>
2015-07-09 16:17:03 +0000

    Improved radar colormap
    The radar colormap is now a proper colormap, which you can get via
    plt.get_cmap('pymeteo_radar') and is available after importing the
    radar module. The contouring levels can be changed via the variable
    cmap_radar_level_step which changes the step used inthe output of
    cmap_radar_levels() and defaults to 0.1, giving 1000 contouring
    levels between -25 and 75 dBZ.

--- pymeteo/radar.py
+++ pymeteo/radar.py
@@ -5,10 +5,11 @@ import matplotlib
 from pylab import pcolor, colorbar
 import os
 
-#translate cmap file to a cmap
+cmap_radar_level_step = 0.1
+"""Used to define the resolution of the color map."""
 
 def cmap_radar_levels():
-   return [-25, 5,10,15,20,25,30, 35, 40, 45, 50, 55, 60, 65, 70, 75]
+   return list(np.arange(-25,75,cmap_radar_level_step))
 
 def cmap_radar_colors():
    return ('#ffffff', # -25  0.0   1.0    1.0    1.0     white
@@ -31,37 +32,18 @@ def cmap_radar_colors():
 def cmap_contour_levels():
    return [30, 40, 50, 60, 65]
 
-def cmap_radar_test():
-   colors =[(0.0, '#ffffff'),
-            (0.3, '#00ecec'),
-            (0.35,'#01A0f6'),
-            (0.4, '#0000f6'),
-            (0.45,'#00ff00'),
-            (0.5, '#00c800'),
-            (0.55,'#009000'),
-            (0.6, '#ffff00'),
-            (0.65,'#e7c000'),
-            (0.7, '#ff9000'),
-            (0.75,'#ff0000'),
-            (0.8, '#d60000'),
-            (0.85,'#c00000'),
-            (0.9, '#ff00ff'),
-            (0.95,'#9955c9'),
-            (1.0, '#ffffff')]
-   return matplotlib.colors.LinearSegmentedColormap.from_list('radar', colors)
-
 def cmap_radar():
    cdict = { 'red' : [(0.0, 1.0, 1.0),
                       (0.3, 1.0, 0.0),
                       (0.35, 0.004, 0.004),
                       (0.4, 0.0, 0.0),
-                      (0.60, 0.0, 1.0),
+                      (0.60, 0.0, 1.0),  
                       (0.65, 0.905, 0.905),
                       (0.7, 1.0, 1.0),
                       (0.75, 1.0, 1.0),
                       (0.8, 0.839, 0.839),
                       (0.85, 0.753, 0.753),
-                      (0.9, 1.0, 1.0),
+                      (0.9, 0.588, 1.0),
                       (0.95, 0.6, 0.6),
                       (1.0, 1.0, 1.0)], 
 
@@ -72,7 +54,7 @@ def cmap_radar():
                       (0.45, 1.0, 1.0),
                       (0.5, 0.784, 0.784),
                       (0.55, 0.6, 0.6),
-                      (0.6, 1.0, 1.0),
+                      (0.6, 0.55, 1.0),
                       (0.65, 0.753, 0.753),
                       (0.7, 0.656, 0.656),
                       (0.75, 0.0, 0.0),
@@ -88,45 +70,9 @@ def cmap_radar():
                       (0.95, 0.788, 0.788), 
                       (1.0 , 1.0, 1.0)] }
 
-   return matplotlib.colors.LinearSegmentedColormap('my_radar', cdict)#, lut=512)
-   #matplotlib.cm.register_cmap(name='pymeteo_radar', data=cdict, lut=512)
+   #return matplotlib.colors.LinearSegmentedColormap('my_radar', cdict)#, lut=512)
+   matplotlib.cm.register_cmap(name='pymeteo_radar', data=cdict, lut=512)
 
 
-def cmap_radar_old():
-   print("loading data")
-
-   v,r1,g1,b1,r2,g2,b2,dbz = np.loadtxt(os.path.join(os.path.dirname(__file__),"cmap_radar.txt"), unpack=True)
-
-   n = len(v)
-   r1= r1/ 255.
-   g1= g1/ 255.
-   b1= b1/ 255.
-   r2= r2/ 255.
-   g2= g2/ 255.
-   b2= b2/ 255.
-   red = []
-   blue = []
-   green = []
-   for i in range(n):
-      red.append((v[i],r1[i],r2[i]))
-      green.append((v[i],g1[i],g2[i]))
-      blue.append((v[i],b1[i],b2[i]))
-
-   red = tuple(red)
-   green = tuple(green)
-   blue = tuple(blue)
-
-   cdict = { 'red' : red, 'green' : green, 'blue' : blue }
-   print(cdict)
-
-   return matplotlib.colors.LinearSegmentedColormap('radar', cdict, 512)
-
-
-def main():
-   print('in main')
-   my_cmap = cmap_radar()
-
-if __name__ == "__main__":
-   print('Started standalon, running main')
-   main()
-
+#setup colormap
+cmap_radar()
