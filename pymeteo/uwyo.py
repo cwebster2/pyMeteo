3f8699f7dca928bacf0509f0fafaab0cf52cf15f
Casey Webster <casey.webster@gmail.com>
2016-01-03 16:04:41 -0600

    Documentation update uwyo.py
    Updated documentation in uwyo module

--- pymeteo/uwyo.py
+++ pymeteo/uwyo.py
@@ -1,4 +1,29 @@
-# uwyo.py
+"""
+.. module: pymeteo.uwyo
+   :platform: Unix, Windows
+   :synopsis: University of Wyoming sounding data helper functions
+
+.. moduleauthor:: Casey Webster <casey.webster@gmail.com>
+
+This module provides functions for working with University of Wyoming sounding
+data.  This includes functions for getting data from files and from the University
+of Wyoming website at http://weather.uwyo.edu/ and a function for preparing that data
+for SkewT plotting.
+
+Getting Data
+++++++++++++
+
+* :py:func:`fetch_from_file` -- loads sounding data from a file 
+* :py:func:`fetch_from_web` -- loads sounding data from website
+
+Preparing Data
+++++++++++++++
+
+* :py:func:`transform_and_check_data` -- quality checks and converts units of data
+
+Module Reference
+++++++++++++++++
+"""
 
 import numpy as np
 import urllib.request as request
@@ -8,6 +33,16 @@ import pymeteo.dynamics as dynamics
 
 
 def fetch_from_file(filename):
+    """Load Uinversity of Wyoming sound data from a file
+
+    :param filename: The filename containing sounding data
+    :type filename: str
+    :returns: pressure, heights, mixing ration, wind direction, wind speed and potential temperature
+
+    This loads sounding data from a file and returns the data columns needed
+    for SkewT plotting.  The data format is the same as copy/pasting data from
+    the uwyo website.
+    """
     with open(filename, 'r') as f:
         title = f.readline()
         skiprows = 7
@@ -21,6 +56,17 @@ def fetch_from_file(filename):
     return (title, p, z, qv, wind_dir, wind_speed, th)
 
 def fetch_from_web(date, station):
+    """Load Uinversity of Wyoming sound data from the uwyo website
+
+    :param date: Time and date of requested sounding data
+    :type date: datetime
+    :param station: The station ID for sounding data
+    :type station: int
+    :returns: pressure, heights, mixing ration, wind direction, wind speed and potential temperature
+
+    This loads sounding data from the web and returns the data columns needed
+    for SkewT plotting.  The date is rounded to the preceding 12-hourly observation at 12Z or 00Z.
+    """
     # http://weather.uwyo.edu/cgi-bin/sounding
     # region=naconf
     # TYPE=TEXT-LIST
@@ -80,6 +126,20 @@ def fetch_from_web(date, station):
 
 
 def transform_and_check_data(p, z, qv, wind_dir, wind_speed, th):
+    """Quality check and convert units of Uinversity of Wyoming sound data
+
+    :param p: pressure
+    :param z: heights
+    :param qv: mixing ration
+    :param wind_dir: wind direction
+    :param wind_speed: wind speed
+    :param th: potential temperature
+    :returns: pressure, heights, mixing ration, u wind, v wind and potential temperature
+
+    This ingests data loaded from file owr web and culls invalid rows (NaN in certain colums)
+    and quality checks the data.  Units are converted from the native uwyo data to the
+    module standard SI units.
+    """
     # clean up NaNs and mark invalid rows for deletion
     nk = len(z)
     for k in np.arange(nk):
