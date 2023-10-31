57c7657718714649e130749c33de48512be91ef9
Casey Webster <cwebster@glgroup.com>
2023-10-31 13:38:09 -0500

    Remove old jq

--- docs-src/source/conf.py
+++ docs-src/source/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
+    'sphinxcontrib.jquery',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
